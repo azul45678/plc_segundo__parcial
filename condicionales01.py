039d196e85bdc0e05117eb22ba29ff560699c2fd
azul45678 <145025981+azul45678@users.noreply.github.com>
2023-09-27 17:33:12 -0600

    condicionales01.py
    ejercicios de programación

---
+++ condicionales01.py
@@ -0,0 +1,24 @@
+EJERCICIO NÚMERO 1
+  #¿Qué le falta?
+
+temp = float(input("Temperatura: "))
+if temp > 90:
+    print("¡Que calor!")
+if temp < 90:
+  print("No mucho calor")
+
+
+EJERCICIO NÚMERO 2 
+#Escribe un programa que reciba un número del usuario e imprima si es positivo, negativo o cero. 
+
+numero = float(input("Ingrese un número: "))
+
+#Utilizar la cadena if/elif/else apropiada, no vayas a emplear tres if consecutivos 
+
+if numero > 0:
+    print("El número es positivo")
+elif numero < 0:
+    print("El número es negativo")
+elif numero == 0:
+  print("El número es 0")
+  

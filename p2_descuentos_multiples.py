ebf229a54dce9b6b9127ef22aadb2e7c80465327
azul45678 <145025981+azul45678@users.noreply.github.com>
2023-10-14 22:27:58 -0600

    p2_descuentos_multiples.py

--- p2_descuentos_multiples.py
+++ p2_descuentos_multiples.py
@@ -1,3 +1,6 @@
+#LINK: https://replit.com/join/pmgnngcryj-azul-esmeraldae
+
+
 #Escribe un programa permita al usuario ingresar el precio de un producto y su categoría (A, B o C). Luego, aplica descuentos en función de la categoría y el número de unidades compradas. Por ejemplo: 
 # Categoría A: 10% descuento
 # Categoría B: 5% descuento
@@ -5,7 +8,7 @@
 
 #Además, si el usuario compra más de 10 unidades, se aplica un descuento_adicional del 5%. Muestra el precio final después de todos los descuentos. 
 
-# Solicitar el precio y la categoría al usuario
+# Pedir el precio y la categoría al usuario
 precio = float(input("Ingresar el precio del producto: "))
 categoria = input("Ingrese la categoría del producto (A (10%, B(5%) o C(2%)): ")
 
@@ -19,7 +22,7 @@ elif categoria == 'C':
 #Pedir la cantidad de unidades
 unidades = int(input("Ingrese la cantidad de unidades compradas: "))
 
-# aplica un descuento adicional del 5% si se compran más de 10 unidades 
+# aaplica descuento adicional del 5% si se compran más de 10 unidades 
 if unidades > 10:
     descuento_adicional = 0.05 
 else:
@@ -31,4 +34,8 @@ descuento_total = descuento + descuento_adicional
 precio_final = precio * (1 - descuento_total)
 
 # PRECIO FINAL 
-print("El precio final es de {}.".format(precio_final))
+
+print("El precio final es de: ",(int(precio_final)))
+
+
+
